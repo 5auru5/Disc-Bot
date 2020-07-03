be161c5e980e50134bc5541c0f75a7d00470fc24
Arya Habibi <5auru5@users.noreply.github.com>
2020-07-03 14:07:10 -0500

    Update GoogleServices.py
    Added options to show by default

--- cogs/GoogleServices.py
+++ cogs/GoogleServices.py
@@ -31,16 +31,22 @@ class GoogleServices(commands.Cog):
         options(string)    
         await ctx.send("This command is being configured")
     
-    @commands.command(name='CreateDoc', help='Create a new Google Doc')
-    async def CreateDoc(self, ctx, string):
+    @commands.command(name='CreateDoc', help='Create and manage Google Docs')
+    async def Docs(self, ctx, string="options"):
+        optionsScreen = "```Here are the commands you can use:\n +Docs Create - Create a Google Doc\n +Docs invite - Invite a user to a Google Doc\n```"
+        await ctx.send(optionsScreen)
         await ctx.send("This command is being configured")
 
-    @commands.command(name='CreateSheet', help='Create a new Google Sheet')
-    async def CreateSheet(self, ctx, string):
+    @commands.command(name='Sheets', help='Create and manage Google Sheets')
+    async def Sheets(self, ctx, string="options"):
+        optionsScreen = "```Here are the commands you can use:\n +Sheets Create - Create a Google Sheet\n +Sheets invite - Invite a user to a Google Sheet\n```"
+        await ctx.send(optionsScreen)
         await ctx.send("This command is being configured")
     
-    @commands.command(name='CreateSlide', help='Create a new Google Slide')
-    async def CreateSlide(self, ctx, string):
+    @commands.command(name='Slides', help='Create and manage Google Slides')
+    async def Slides(self, ctx, string="options"):
+        optionsScreen = "```Here are the commands you can use:\n +Slides Create - Create a Google Slide\n +Slides invite - Invite a user to a Google Slide\n```"
+        await ctx.send(optionsScreen)
         await ctx.send("This command is being configured")
 def setup(bot):
     bot.add_cog(GoogleServices(bot))
