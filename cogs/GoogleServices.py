0a513926dabfbba2179e345eef888c58f04a3723
Arya Habibi <5auru5@users.noreply.github.com>
2020-07-03 14:11:40 -0500

    Update GoogleServices.py

--- cogs/GoogleServices.py
+++ cogs/GoogleServices.py
@@ -10,7 +10,7 @@ class GoogleServices(commands.Cog):
 
     @commands.command(name='Calendar', help='Create and manage Google Calendar events')
     async def Calendar(self, ctx, string="options"):
-        if string = "options":
+        if string == "options":
             optionsScreen = "```Here are the commands you can use:\n +Calendar Create - Create a calendar\n +Calendar edit - Edit a calendar\n +Calendar delete - Delete a calendar event\n +Calendar invite - Invite a user to a calendar```"
             await ctx.send(optionsScreen)
         def create():
@@ -34,21 +34,21 @@ class GoogleServices(commands.Cog):
     
     @commands.command(name='Docs', help='Create and manage Google Docs')
     async def Docs(self, ctx, string="options"):
-        if string = "options":
+        if string == "options":
             optionsScreen = "```Here are the commands you can use:\n +Docs Create - Create a Google Doc\n +Docs invite - Invite a user to a Google Doc\n```"
             await ctx.send(optionsScreen)
         await ctx.send("NOTE: This command is being configured")
 
     @commands.command(name='Sheets', help='Create and manage Google Sheets')
     async def Sheets(self, ctx, string="options"):
-        if string = "options":
+        if string == "options":
             optionsScreen = "```Here are the commands you can use:\n +Sheets Create - Create a Google Sheet\n +Sheets invite - Invite a user to a Google Sheet\n```"
             await ctx.send(optionsScreen)
         await ctx.send("NOTE: This command is being configured")
     
     @commands.command(name='Slides', help='Create and manage Google Slides')
     async def Slides(self, ctx, string="options"):
-        if string = "options":
+        if string == "options":
             optionsScreen = "```Here are the commands you can use:\n +Slides Create - Create a Google Slide\n +Slides invite - Invite a user to a Google Slide\n```"
             await ctx.send(optionsScreen)
         await ctx.send("NOTE: This command is being configured")
