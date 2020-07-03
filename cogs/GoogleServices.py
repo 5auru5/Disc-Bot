a724d3b74a4976251f393fa238bc80d2c15ac892
Arya Habibi <5auru5@users.noreply.github.com>
2020-07-03 14:08:15 -0500

    Update GoogleServices.py

--- cogs/GoogleServices.py
+++ cogs/GoogleServices.py
@@ -29,25 +29,25 @@ class GoogleServices(commands.Cog):
             }
             return switcher.get(string)
         options(string)    
-        await ctx.send("This command is being configured")
+        await ctx.send("NOTE: This command is being configured")
     
-    @commands.command(name='CreateDoc', help='Create and manage Google Docs')
+    @commands.command(name='Doc', help='Create and manage Google Docs')
     async def Docs(self, ctx, string="options"):
         optionsScreen = "```Here are the commands you can use:\n +Docs Create - Create a Google Doc\n +Docs invite - Invite a user to a Google Doc\n```"
         await ctx.send(optionsScreen)
-        await ctx.send("This command is being configured")
+        await ctx.send("NOTE: This command is being configured")
 
     @commands.command(name='Sheets', help='Create and manage Google Sheets')
     async def Sheets(self, ctx, string="options"):
         optionsScreen = "```Here are the commands you can use:\n +Sheets Create - Create a Google Sheet\n +Sheets invite - Invite a user to a Google Sheet\n```"
         await ctx.send(optionsScreen)
-        await ctx.send("This command is being configured")
+        await ctx.send("NOTE: This command is being configured")
     
     @commands.command(name='Slides', help='Create and manage Google Slides')
     async def Slides(self, ctx, string="options"):
         optionsScreen = "```Here are the commands you can use:\n +Slides Create - Create a Google Slide\n +Slides invite - Invite a user to a Google Slide\n```"
         await ctx.send(optionsScreen)
-        await ctx.send("This command is being configured")
+        await ctx.send("NOTE: This command is being configured")
 def setup(bot):
     bot.add_cog(GoogleServices(bot))
 
