86d071e81fbdc25962f0dd068ece0f2976f5c82f
Arya Habibi <5auru5@users.noreply.github.com>
2020-10-26 18:54:35 -0500

    Music Request
    new service

---
+++ cogs/MusicRequest.py
@@ -0,0 +1,24 @@
+#internalcommands.py
+import os
+import time
+import datetime
+from discord.ext import commands
+
+start_time = time.time()
+class MusicRequest(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='pull', help='Pull Hydra to your currect VC')
+    async def status(self, ctx, arg1):
+        if lower(arg1) == "lofi":
+            await ctx.sned("Pulling LoFi Radio to your current VC")
+        elif lower(arg1) == "synthwave":
+            await ctx.send("Pulling Synthwave Radio to your current VC")
+        else:
+            await ctx.send("The current Radio stations you can pull are:\n Synthwave\n LoFi\n ")
+        await ctx.send(response)
+
+def setup(bot):
+    bot.add_cog(InternalCommands(bot))
+
